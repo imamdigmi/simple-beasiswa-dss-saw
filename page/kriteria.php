6012f41a331984257e5c7a2a3b4069207e9915fd
imam digmi <imam.digmi@gmail.com>
2016-12-30 00:12:33 +0700

    ADD: kriteria

---
+++ page/kriteria.php
@@ -0,0 +1,93 @@
+<?php
+$update = (isset($_GET['action']) AND $_GET['action'] == 'update') ? true : false;
+if ($update) {
+	$sql = $connection->query("SELECT * FROM kriteria WHERE kd_kriteria='$_GET[key]'");
+	$row = $sql->fetch_assoc();
+}
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+	if ($update) {
+		$sql = "UPDATE kriteria SET nama='$_POST[nama]', sifat='$_POST[sifat]' WHERE kd_kriteria='$_GET[key]'";
+	} else {
+		$sql = "INSERT INTO kriteria VALUES ('$_POST[kd_kriteria]', '$_POST[nama]', '$_POST[sifat]')";
+	}
+  if ($connection->query($sql)) {
+    echo alert("Berhasil!", "?page=kriteria");
+  } else {
+		echo alert("Gagal!", "?page=kriteria");
+  }
+}
+
+if (isset($_GET['action']) AND $_GET['action'] == 'delete') {
+  $connection->query("DELETE FROM kriteria WHERE kd_kriteria='$_GET[key]'");
+	echo alert("Berhasil!", "?page=kriteria");
+}
+?>
+<div class="row">
+	<div class="col-md-4">
+	    <div class="panel panel-<?= ($update) ? "warning" : "info" ?>">
+	        <div class="panel-heading"><h3 class="text-center"><?= ($update) ? "EDIT" : "TAMBAH" ?></h3></div>
+	        <div class="panel-body">
+	            <form action="<?=$_SERVER['REQUEST_URI']?>" method="POST">
+	                <div class="form-group">
+	                    <label for="kd_kriteria">KODE</label>
+	                    <input type="text" name="kd_kriteria" class="form-control" <?= (!$update) ?: 'value="'.$row["kd_kriteria"].'" disabled="on"' ?>>
+	                </div>
+	                <div class="form-group">
+	                    <label for="nama">Nama</label>
+	                    <input type="text" name="nama" class="form-control" <?= (!$update) ?: 'value="'.$row["nama"].'"' ?>>
+	                </div>
+									<div class="form-group">
+	                  <label for="nama">Sifat</label>
+										<select class="form-control" name="sifat">
+											<option>---</option>
+											<option value="min" <?= (!$update) ?: (($row["sifat"] != "min") ?: 'selected="on"') ?>>Min</option>
+											<option value="max" <?= (!$update) ?: (($row["sifat"] != "max") ?: 'selected="on"') ?>>Max</option>
+										</select>
+									</div>
+	                <button type="submit" class="btn btn-<?= ($update) ? "warning" : "info" ?> btn-block">Simpan</button>
+	                <?php if ($update): ?>
+										<a href="?page=kriteria" class="btn btn-info btn-block">Batal</a>
+									<?php endif; ?>
+	            </form>
+	        </div>
+	    </div>
+	</div>
+	<div class="col-md-8">
+	    <div class="panel panel-info">
+	        <div class="panel-heading"><h3 class="text-center">DAFTAR</h3></div>
+	        <div class="panel-body">
+	            <table class="table table-condensed">
+	                <thead>
+	                    <tr>
+	                        <th>No</th>
+	                        <th>Kode</th>
+	                        <th>Nama</th>
+	                        <th>Sifat</th>
+	                        <th></th>
+	                    </tr>
+	                </thead>
+	                <tbody>
+	                    <?php $no = 1; ?>
+	                    <?php if ($query = $connection->query("SELECT * FROM kriteria")): ?>
+	                        <?php while($row = $query->fetch_assoc()): ?>
+	                        <tr>
+	                            <td><?=$no++?></td>
+	                            <td><?=$row['kd_kriteria']?></td>
+	                            <td><?=$row['nama']?></td>
+	                            <td><?=$row['sifat']?></td>
+	                            <td>
+	                                <div class="btn-group">
+	                                    <a href="?page=kriteria&action=update&key=<?=$row['kd_kriteria']?>" class="btn btn-warning btn-xs">Edit</a>
+	                                    <a href="?page=kriteria&action=delete&key=<?=$row['kd_kriteria']?>" class="btn btn-danger btn-xs">Hapus</a>
+	                                </div>
+	                            </td>
+	                        </tr>
+	                        <?php endwhile ?>
+	                    <?php endif ?>
+	                </tbody>
+	            </table>
+	        </div>
+	    </div>
+	</div>
+</div>
